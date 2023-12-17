92ed8079efa8556eddaf4776efe713aeeec13ed9
Yashi Bagoli <31111010+yashi32@users.noreply.github.com>
2023-12-17 22:30:54 +0530

    Update Hoisting.js

--- Hoisting.js
+++ Hoisting.js
@@ -1,5 +1,7 @@
 /Hoisting in JavaScript
 
+/Hoisting is a phenomenon in JS by which you can access functions and variables even before you have initialized.You can access it without error.
+
 getName();
 console.log(x);
 
